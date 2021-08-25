da7b087f83072a1ba1393753742835726d93c216
Btmartone <btmartone@gmail.com>
2021-08-24 20:39:24 -0400

    add/run ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,22 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
     }
 }
 
 tasks.register<Delete>("clean") {
     this.delete.add(rootProject.buildDir)
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
